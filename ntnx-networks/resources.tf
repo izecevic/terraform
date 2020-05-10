36ec1908d85c71962db4e61d36b358ccdf5f37be
Igor Zecevic <igor.zecevic@nutanix.com>
2020-05-10 17:58:30 +0200

    commit

---
+++ ntnx-networks/resources.tf
@@ -0,0 +1,22 @@
+provider "nutanix" {
+  username = var.prismUser
+  password = var.prismSecret
+  endpoint = var.prismEndpoint
+  insecure = true
+  port     = var.prismPort
+}
+
+data "nutanix_clusters" "clusters" {}
+locals {
+  cluster = data.nutanix_clusters.clusters.entities[0].metadata.uuid
+}
+
+# resource network
+resource "nutanix_subnet" "networks" {
+  count        = length(var.networks)
+  cluster_uuid = local.cluster
+  subnet_type  = "VLAN"
+  name         = lookup(var.networks[count.index], "name")
+  vlan_id      = lookup(var.networks[count.index], "id")
+  vswitch_name = lookup(var.networks[count.index], "bridge")
+}
