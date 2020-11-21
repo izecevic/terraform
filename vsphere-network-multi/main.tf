781599d958f439ac91d9409f6eae484afed965db
igor.zecevic@nutanix.com <igor.zecevic@nutanix.com>
2020-11-21 12:13:22 +0100

    yes

--- vsphere-network-multi/main.tf
+++ vsphere-network-multi/main.tf
@@ -9,20 +9,20 @@ provider "vsphere" {
 }
 
 # Datasources 
-data "vsphere_datacenter" "dc_esgi" {
+data "vsphere_datacenter" "datacenter" {
   name = var.vsphere_datacenter
 }
 
-data "vsphere_host" "my_esxi" {
+data "vsphere_host" "host" {
   name           = var.vsphere_esxi_name
-  datacenter_id  = data.vsphere_datacenter.dc_esgi.id
+  datacenter_id  = data.vsphere_datacenter.dacenter.id
 }
 
 # Resource
-resource "vsphere_host_port_group" "pg_esgi1" {
+resource "vsphere_host_port_group" "portgroups" {
   for_each            = var.vsphere_portgroups
   name                = each.key
-  host_system_id      = data.vsphere_host.my_esxi.id 
+  host_system_id      = data.vsphere_host.host.id 
   virtual_switch_name = var.vswitch_name 
   vlan_id             = each.value
 }
